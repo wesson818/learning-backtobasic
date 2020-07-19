2af99351bcc70ba7a17162da2d87f9770c5e7a41
WenJing Zhang <wesson818@gmail.com>
2020-07-19 18:52:49 +1000

    array methods

---
+++ JavaScript/ArrayMethods.js
@@ -0,0 +1,71 @@
+// JavaScript Array Methods
+const items = [{
+        name: 'ball',
+        points: 2
+    },
+    {
+        name: 'coin',
+        points: 3
+    },
+    {
+        name: 'candy',
+        points: 4
+    }
+];
+
+const numbers = [1,2,3,4,5]
+
+// filter
+const filterResult = items.filter((item) => {
+    return item.points <= 3
+})
+console.log("filterResult",filterResult)
+//filterResult [{name: 'ball',points: 2},{name: 'coin',points: 3}]
+
+// map
+const mapResult = items.map((item) => {
+    return item.name
+})
+console.log("mapResult",mapResult)
+//mapResult ['ball','coin','candy']
+
+// reduce
+const reduceResult = items.reduce((currentTotal, item)=>{
+    return item.points + currentTotal
+},0)
+console.log("reduceResult",reduceResult)
+//"reduceResult" 9
+
+// find
+const findResult = items.find((item)=>{
+    return item.name === 'coin'
+})
+console.log("findResult",findResult)
+//findResult {name: 'coin', points: 3}
+
+// forEach
+items.forEach((item)=>{
+    console.log("forEachResult",item.name)
+})
+//forEachResult ball
+//forEachResult coin
+//forEachResult candy
+
+// some
+const someResult = items.some((item)=>{
+    return item.points < 4
+})
+console.log("someResult",someResult)
+//someResult true
+
+// every
+const everyResult = items.every((item) => {
+    return item.points <= 5
+})
+console.log("everyResult",everyResult)
+//everyResult true
+
+// includes
+const includesResult = numbers.includes(7)
+console.log("includesResult",includesResult)
+//includesResult false
