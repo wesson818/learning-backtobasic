534990a04f2a6920e0eca981680475d42ac9fe77
Wesson Zhang <>
2020-08-13 23:36:08 +1000

    clear input text

--- ReactNative/playground/components/AddItem.js
+++ ReactNative/playground/components/AddItem.js
@@ -4,19 +4,23 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome'
 
 export default AddItem = ({addItem}) => {
   const [text, setText] = useState('')
-//   const inputRef = useRef()
+  const inputRef = useRef()
 
-
+  // todo: make clear work
+  function clearInput(){
+    inputRef._root.clear();
+  }
   return (
     <View style={styles.header}>
       <TextInput 
-        // ref={inputRef}
+        ref={inputRef}
         placeholder="Add Item..." 
         style={styles.input}  
         onChangeText={(textValue)=>{setText(textValue)}}
+        clearButtonMode="always"
       />
       <Text>{text}</Text>
-      <TouchableOpacity style={styles.btn} onPress={()=>{addItem(text)}}>
+      <TouchableOpacity style={styles.btn} onPress={()=>{clearInput,addItem(text)}}>
         <Text style={styles.btnText}>
            <Icon name="plus" size={20} /> Add Item
         </Text>
