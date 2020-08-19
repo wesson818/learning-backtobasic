4eac4ccecbd49ab4e12052f0be3e10d11d34b298
WenJing Zhang <wesson818@gmail.com>
2020-08-19 20:32:20 +1000

    update

--- JavaScript/objectIterator.js
+++ JavaScript/objectIterator.js
@@ -1,11 +1,11 @@
 const person = {
     id:1,
-    firstname:"Wen",
-    lastname:"Zhang",
-    age:35,
+    firstname:"John",
+    lastname:"Smith",
+    age:25,
     male: true,
     job:"developer",
-    salay:"120000"
+    salay:"90000"
 }
 
 //loop key in object using for in loop
@@ -13,8 +13,9 @@ for(const key in person){
     console.log(`${key}:${person[key]}`)
 }
 
-//turn object to value array using Object.values(), return new array
+// turn object to value array using Object.values(), return new array
 console.log('Object.values(person) :', Object.values(person));
+// [ 1, 'Wen', 'Zhang', 35, true, 'developer', '120000' ]
 const getNumber = (obj) => {
     return Object.values(obj).filter(val => !isNaN(val) && typeof val!=='boolean')
 }
@@ -22,6 +23,7 @@ console.log("getNumber:", getNumber(person))
 
 const numberArray = Object.values(person).filter(val => !isNaN(val) && typeof(val)!=='boolean')
 console.log('numberArray :', numberArray);
+// numberArray : [ 1, 35, '120000' ]
 
 //turn object to 2D array key value pairs using Object.entries, return new object
 console.log("Object.entries(person):",Object.entries(person))
@@ -33,3 +35,15 @@ for(const [key, value] of Object.entries(person)){
     }
 }
 console.log('newObj :', newObj);
+
+// use console.time and console.timeEnd to tracking run time
+const benchmarkObj = size => {
+    console.time('benchmarkObj');
+    var obj = {};
+    for (var i = 0; i < size; i++) obj[i] = i;
+    for (var i = 0; i < size; i++) var x = obj[i];
+    console.timeEnd('benchmarkObj');
+  }
+  
+  var size = 1000000;
+  benchmarkObj(size);
