345d9e23160a1e13fc3da9619fb9b150c6e64e32
wesson818 <wesson818@gmail.com>
2020-09-13 14:23:21 +1000

    life cycle hooks

--- Angular/component-interaction/src/app/app.component.ts
+++ Angular/component-interaction/src/app/app.component.ts
@@ -7,6 +7,50 @@ import { InteractionService } from './interaction.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// life cycle hooks
+
+// ngOnChanges(changes: SimpleChanges): void {
+//   //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+//   //Add '${implements OnChanges}' to the class.
+// }
+
+// ngOnInit(): void {
+//   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+//   //Add 'implements OnInit' to the class.
+// }
+
+// ngDoCheck(): void {
+//   //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+//   //Add 'implements DoCheck' to the class.
+// }
+
+// ngAfterContentInit(): void {
+//   //Called after ngOnInit when the component's or directive's content has been initialized.
+//   //Add 'implements AfterContentInit' to the class.
+// }
+
+// ngAfterContentChecked(): void {
+//   //Called after every check of the component's or directive's content.
+//   //Add 'implements AfterContentChecked' to the class.
+// }
+
+// ngAfterViewInit(): void {
+//   //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+//   //Add 'implements AfterViewInit' to the class.
+// }
+
+// ngAfterViewChecked(): void {
+//   //Called after every check of the component's view. Applies to components only.
+//   //Add 'implements AfterViewChecked' to the class.
+// }
+
+// ngOnDestroy(): void {
+//   //Called once, before the instance is destroyed.
+//   //Add 'implements OnDestroy' to the class.
+// }
+
+
 export class AppComponent implements AfterViewInit {
   pageTitle = "Angular Component Interaction";
   imgUrl = "https://picsum.photos/200";
