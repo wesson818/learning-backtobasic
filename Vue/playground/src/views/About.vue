12b9f66a2de06b1f8c76f3fb759651370a96ac23
wesson818 <wesson818@gmail.com>
2020-11-13 00:06:32 +1100

    vue3.0 setup

---
+++ Vue/playground/src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>{{ collectionName }}: {{ readersNumber }} {{ book.title }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { reactive, ref } from 'vue';
+
+export default {
+    props: {
+      collectionName: String
+    },
+    setup(props) {
+      const readersNumber = ref(0)
+      const book = reactive({ title: 'Vue 3 Guide' })
+
+      // expose to template
+      return {
+        readersNumber,
+        book
+      }
+    }
+}
+</script>
