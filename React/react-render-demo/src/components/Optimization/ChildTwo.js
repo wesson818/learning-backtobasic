d8cbe9ba5f644cf425390bf8105923d05e22a21b
Wesson <wesson818@gmail.com>
2021-08-21 15:55:08 +1000

    react memo

--- React/react-render-demo/src/components/Optimization/ChildTwo.js
+++ React/react-render-demo/src/components/Optimization/ChildTwo.js
@@ -10,4 +10,9 @@ export const ChildTwo = ({name}) => {
     )
 }
 
+// React.memo is HOC for performance 
+// In React, when a parent component renders, a child component might un-necessarily render.
+// To optimize this behaviour, you can use React.memo and pass in the child component 
+// React.memo will perform a shallow comparison of the previous and new props and 
+// re-render the child component only if the props have changed.
 export const MemoChildTwo = React.memo(ChildTwo)
