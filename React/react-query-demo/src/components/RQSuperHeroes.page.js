52756defcdfe2568b6216bbeba495081cbc39aec
Wesson <wesson818@gmail.com>
2022-03-23 21:38:09 +1100

    learn more react query

--- React/react-query-demo/src/components/RQSuperHeroes.page.js
+++ React/react-query-demo/src/components/RQSuperHeroes.page.js
@@ -1,21 +1,41 @@
 import { useQuery } from 'react-query'
 import axios from 'axios'
+import { useState } from 'react'
 
 const fetchSuperHeroes = () => 
   axios.get('http://localhost:4000/superheroes')
 
 export const RQSuperHeroesPage = () => {
-    const {isLoading, data, isError, error} = useQuery('super-heroes', fetchSuperHeroes)
+    const [refetchTimes, setRefetchTimes] = useState(0);
+    const [intervalTime, setIntervalTime] = useState(2000);
+    const onSuccess = data => {
+      console.log('onSuccess', data);
+      setRefetchTimes(refetchTimes + 1);
+      if (refetchTimes >= 3) setIntervalTime(10000);
+    }
+    const onError = error => {
+      console.log('onError', error);
+    }
+    const {isLoading, data, isError, error, isFetching, refetch} = 
+      useQuery('super-heroes', fetchSuperHeroes, {
+        enabled: false,
+        // need enable: true to make refetchInterval work
+        refetchInterval: intervalTime,
+        refetchIntervalInBackground: true,
+        onSuccess,
+        onError,
+      })
 
-    if (isLoading) <h2>Loading...</h2>
+    if (isLoading || isFetching) <h2>Loading...</h2>
 
     if (isError) <h2>{error.message}</h2>
 
     return (
       <>
         <h2>RQ Super Heroes Page</h2>
+        <button onClick={refetch}>Fetch Data</button>
         {data?.data.map(hero => {
-          return <div>{hero.name}</div>
+          return <div key={hero.id}>{hero.name}</div>
         })}
       </>
     )    
